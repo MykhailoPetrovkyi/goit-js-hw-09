83abbd357fc5e9049f506db8644243bc38737b8b
MykhailoPetrovkyi <petrovskij65@gmail.com>
2024-12-25 18:49:28 +0200

    Init

---
+++ src/js/2-form.js
@@ -0,0 +1,42 @@
+const form = document.querySelector('.feedback-form');
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const fillFormData = () => {
+  try {
+    if (localStorage.length === 0) {
+      return;
+    }
+
+    const dataLocale = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+    formData = dataLocale;
+
+    for (const key in dataLocale) {
+      form.elements[key].value = dataLocale[key];
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fillFormData();
+
+form.addEventListener('change', event => {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(formData);
+  if (formData.email !== '' && formData.message !== '') {
+    event.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+  } else {
+    alert('Fill please all fields');
+  }
+});
